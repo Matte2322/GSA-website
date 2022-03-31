37f8979dc723501ba93c04e3383cfe2ccb9772b9
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-03-31 16:27:53 -0700

    Added font colors for light and dark mode.
    YEP.

--- magic.js
+++ magic.js
@@ -8,14 +8,24 @@ const darkTheme = () => {
     var darkTheme = document.getElementById('background');
     darkTheme.style.backgroundColor = '#212121';
 };
+const fontColorLightMode = () => {
+    var fontColor = document.getElementById('background');
+    fontColor.style.color = '#212121';
+};
+const fontColorDarkMode = () => {
+    var fontColor = document.getElementById('background');
+    fontColor.style.color = '#f0ffff';
+};
 
 icon.onclick = function() {
     document.body.classList.toggle("moon-icon");
     if(document.body.classList.toggle('background')) {
         icon.src = 'images/sun.png';
         darkTheme();
+        fontColorDarkMode();
     }else{
         icon.src = 'images/moon.png';
         lightTheme();
+        fontColorLightMode();
     }
 }
