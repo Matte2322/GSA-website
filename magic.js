5fc46bbe3042cf4422ac0062c3542c769e1aca18
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-03-30 22:52:49 -0700

    Add files via upload
    I just uploaded it and I am done for the day. I will continue doing it tomorrow.
    Successfully got it to look kinda like a website.

---
+++ magic.js
@@ -0,0 +1,21 @@
+var icon = document.getElementById("moon-icon");
+
+const lightTheme = () => {
+    var darkTheme = document.getElementById('background');
+    darkTheme.style.backgroundColor = '#f0ffff';
+};
+const darkTheme = () => {
+    var darkTheme = document.getElementById('background');
+    darkTheme.style.backgroundColor = '#212121';
+};
+
+icon.onclick = function() {
+    document.body.classList.toggle("moon-icon");
+    if(document.body.classList.toggle('background')) {
+        icon.src = 'images/sun.png';
+        darkTheme();
+    }else{
+        icon.src = 'images/moon.png';
+        lightTheme();
+    }
+}
